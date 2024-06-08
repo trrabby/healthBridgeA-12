832fd452a4d22608d5f358e53a9ea1a3880c1e00
trrabby1@gmail.com <110276063+trrabby@users.noreply.github.com>
2024-06-08 11:41:20 +0600

    added sort func

--- src/Pages/AvailableCamp/AvailableCamp.jsx
+++ src/Pages/AvailableCamp/AvailableCamp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { LoadingSpinnerCircle } from '../../Components/LoadingSpinnerCircle'
 import { useAllCamps } from '../../Hooks/useAllCamps';
@@ -6,18 +6,24 @@ import { SectionHead } from '../../Components/SectionHead';
 import { Camp } from '../../Components/Camp';
 import { Button } from '@headlessui/react';
 import { useAxiosCommon } from '../../Hooks/useAxiosCommon';
+import toast from 'react-hot-toast';
 
 
 export const AvailableCamp = () => {
 
-  const { camps, isLoading, refetch } = useAllCamps();
-  // console.log(camps)
-  
-  const [filter, setFilter] = useState(null)
-  
-  // console.log(filter)
+  const { camps , isLoading, refetch } = useAllCamps();
+ 
 
-  const axiosCommon=useAxiosCommon()
+  const [campsData, setCampsData] = useState([])
+  const [filter, setFilter] = useState(null)
+
+  console.log(campsData)
+
+  useEffect(()=>{
+    setCampsData(camps)
+  },[camps])
+
+  const axiosCommon = useAxiosCommon()
   // console.log(isLoading)
 
   if (isLoading) {
@@ -31,7 +37,7 @@ export const AvailableCamp = () => {
     // console.log(value)
     // setValue(value)
 
-    if (value ==="") {
+    if (value === "") {
       return setFilter(null)
     }
 
@@ -39,8 +45,33 @@ export const AvailableCamp = () => {
     setFilter(data)
     toast.success(`${data.length} ${data.length > 1 ? "Blogs" : "Blog"} found`)
 
+  }
 
+  const handleFrontSel = async (e) => {
+    const value = e.target.value
+    console.log(value)
 
+    
+    
+
+    if (value === "Random") {
+      return setCampsData(camps)
+    }
+
+    else if (value === "Most_Registered") {
+      const {data} = await axiosCommon('/mostReg')
+      setCampsData(data)
+    }   
+
+    else if (value === "Camp_Fees") {
+      const {data} = await axiosCommon('/camp_fee')
+      setCampsData(data)
+    }  
+
+    else if (value === "Alphabetical_Order") {
+      const {data} = await axiosCommon('/Alphabetical_Order')
+      setCampsData(data)
+    }   
 
   }
   return (
@@ -51,17 +82,26 @@ export const AvailableCamp = () => {
       ></SectionHead>
 
       <div className='flex items-center justify-center bg-accent p-5 rounded-lg gap-2'>
-        <input onKeyUp={handleTextSearch} type="text" name="searchField" className=' p-2 px-4 w-4/12 rounded-lg' placeholder='Search Your Desired Camp Here' /> 
-        
-        <Button 
+        <input onKeyUp={handleTextSearch} type="text" name="searchField" className=' p-2 px-4 w-4/12 rounded-lg' placeholder='Search Your Desired Camp Here' />
 
-        className="bg-primary p-2 rounded-xl font-bold hover:scale-105 duration-500 hover:bg-third hover:text-white">Search</Button>
+        <Button
+
+          className="bg-primary p-2 rounded-xl font-bold hover:scale-105 duration-500 hover:bg-third hover:text-white">Search</Button>
+
+        <select name="selectionTab" onChange={handleFrontSel} className='w-full h-full border-none bg-none outline-none text-sm lg:text-base' placeholder="Select">
+          <option value="" disabled selected>Sort By</option>
+          <option value="Random">Random</option>
+          <option value="Most_Registered">Most Registered</option>
+          <option value="Camp_Fees">Camp Fees</option>
+          <option value="Alphabetical_Order">Alphabetical Order</option>
+
+        </select>
       </div>
 
       <div className='grid grid-flow-row grid-cols-1 lg:grid-cols-2 justify-between gap-5 p-0 mb-10 lg:w-full w-10/12 mx-auto mt-5 '>
-        { 
-          filter? filter.map(camp => <Camp key={camp._id} camp={camp} refetch={refetch}></Camp>) :
-          camps.map(camp => <Camp key={camp._id} camp={camp} refetch={refetch}></Camp>)
+        {
+          filter ? filter.map(camp => <Camp key={camp._id} camp={camp} refetch={refetch}></Camp>) :
+            campsData?.map(camp => <Camp key={camp._id} camp={camp} refetch={refetch}></Camp>)
         }
       </div>
     </div>
